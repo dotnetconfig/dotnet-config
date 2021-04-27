05ef8ede65d8891daf8a2c8d27d000fa50c80f42
Daniel Cazzulino <daniel@cazzulino.com>
2021-04-27 14:24:01 -0300

    ⚙ Add basic Microsoft.Extensions.Configuration support
    Basic read/write is supported, alongside hierarchical loading.
    
    Given a dotnet-config like:
    
    ```editorconfig
    [http "https://foo.com"]
       sslVerify = false
    ```
    
    You can retrieve the value with the following key:
    
    ```csharp
    var config = new ConfigurationBuilder().AddDotNetConfig().Build();
    var ssl = config["http:https://foo.com:sslVerify"];
    ```
    
    So section/subsection/variable are fully supported.
    
    Monitoring files and reloading is not supported at this time.
    
    Fixes #3.

---
+++ src/Configuration/DotNetConfigSource.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DotNetConfig
+{
+    class DotNetConfigSource : IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder) => new DotNetConfigProvider();
+    }
+}
