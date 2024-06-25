30aa5b2911586faaa8c1f65540f5981d6178d7a1
Daniel Cazzulino <daniel@cazzulino.com>
2024-06-25 15:12:31 -0300

    Add test that ensures current tab-based behavior
    To ensure compatibility with git config.
    
    Closes #122

--- src/Config.Tests/ConfigTests.cs
+++ src/Config.Tests/ConfigTests.cs
@@ -250,6 +250,19 @@ namespace DotNetConfig
         }
 
         [Fact]
+        public void when_setting_variable_then_uses_tab()
+        {
+            var file = Path.GetTempFileName();
+            var config = Config.Build(file);
+
+            config.SetString("section", "subsection", "foo", "bar");
+
+            var line = File.ReadAllLines(file).SkipWhile(x => x[0] == '#' || x[0] == '[').First();
+
+            Assert.Equal('\t', line[0]);
+        }
+
+        [Fact]
         public void when_setting_global_variable_then_writes_global_file()
         {
             Config.GlobalLocation = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString(), ".netconfig");
