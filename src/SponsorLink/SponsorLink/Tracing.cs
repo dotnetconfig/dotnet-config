9a732b0582d0196f64eaf880b5400041d5f05865
kzu <kzu@users.noreply.github.com>
2024-06-30 01:10:14 +0000

    ⬆️ Bump files with dotnet-file sync # devlooped/oss
    - Introduce lazy-init of sponsoring status, simplify diagnostics https://github.com/devlooped/oss/commit/5009784
    - Update to newest JsonWebTokens https://github.com/devlooped/oss/commit/068140b
    - Extend grace period to unknown status too https://github.com/devlooped/oss/commit/9f918ec
    - Add support and showcase determining install time https://github.com/devlooped/oss/commit/717ddb1
    - Fix formatting/whitespace https://github.com/devlooped/oss/commit/7febebc
    - Add SponsorLinkImported so we can skip imports https://github.com/devlooped/oss/commit/c81f532
    - Make sure Funding class is available to intellisense https://github.com/devlooped/oss/commit/5813f21
    - Make sure we report only once per product for entire solution https://github.com/devlooped/oss/commit/4b7f922
    - Change debug traces location to the well-known location of .sponsorlink https://github.com/devlooped/oss/commit/1019e2a
    - Remove unused tracing overloads https://github.com/devlooped/oss/commit/08a8488
    - Fix roles checking from new identity-based token handler https://github.com/devlooped/oss/commit/6eecf46
    - Switch to built-in item metadata for manifest analyzer files https://github.com/devlooped/oss/commit/49c9a38
    - Replace JWT package in tests targets too https://github.com/devlooped/oss/commit/ba1310c

--- src/SponsorLink/SponsorLink/Tracing.cs
+++ src/SponsorLink/SponsorLink/Tracing.cs
@@ -10,12 +10,6 @@ namespace Devlooped.Sponsors;
 
 static class Tracing
 {
-    public static void Trace(string message, object? value, [CallerArgumentExpression("value")] string? expression = null, [CallerFilePath] string? filePath = null, [CallerLineNumber] int lineNumber = 0)
-        => Trace($"{message}: {value} ({expression})", filePath, lineNumber);
-
-    public static void Trace(object? value, [CallerArgumentExpression("value")] string? expression = null, [CallerFilePath] string? filePath = null, [CallerLineNumber] int lineNumber = 0)
-        => Trace($"{value} ({expression})", filePath, lineNumber);
-
     public static void Trace([CallerMemberName] string? message = null, [CallerFilePath] string? filePath = null, [CallerLineNumber] int lineNumber = 0)
     {
         var trace = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SPONSORLINK_TRACE"));
@@ -33,14 +27,16 @@ static class Tracing
             .AppendLine($" -> {filePath}({lineNumber})")
             .ToString();
 
-        var dir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create);
+        var dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".sponsorlink");
+        Directory.CreateDirectory(dir);
+
         var tries = 0;
         // Best-effort only
         while (tries < 10)
         {
             try
             {
-                File.AppendAllText(Path.Combine(dir, "SponsorLink.log"), line);
+                File.AppendAllText(Path.Combine(dir, "trace.log"), line);
                 Debugger.Log(0, "SponsorLink", line);
                 return;
             }
